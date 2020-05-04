a71af967450348abe1c4d8025e0c4ba1d23dcc79
Akitotameto <akito.tameto@gmail.com>
2020-05-04 17:57:29 +0900

    react-router-domをインストールしてRouteを設定

---
+++ src/pages/set.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Set extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Setテスト</h1>
+            </div>
+        )
+    }
+}
+
+export default Set;
