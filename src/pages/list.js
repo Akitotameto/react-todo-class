8b70f60a5138e18ac946faf8034aa026f3fd947e
Akitotameto <akito.tameto@gmail.com>
2020-05-04 20:10:51 +0900

    TodoDateLowクラスを追加

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 import Header from  '../components/header'
+import TodoDateLow from  '../components/todo-date-low'
+
+const todoData = [
+    {
+        id: 1,
+        title: 'タイトル1',
+        content: '内容1内容1内容1内容1内容1内容1内容1',
+        createdAt: '2020-05-04',
+        category: 1,
+    },
+    {
+        id: 2,
+        title: 'タイトル2',
+        content: '内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2内容2',
+        createdAt: '2020-05-04',
+        category: 2,
+    },
+    {
+        id: 3,
+        title: 'タイトル3',
+        content: '内容3内容3内容3内容3内容3内容3内容3内容3内容3内容3内容3内容3',
+        createdAt: '2020-05-04',
+        category: 1,
+    },
+]
 
 class List extends Component {
+
     render() {
         return(
             <div>
-                <Header history={this.props.history}/>
+                <Header history={this.props.history} index={1}/>
                 <div className='contents-1'>
                     <h1>Todo List</h1>
                 </div>
@@ -15,16 +41,11 @@ class List extends Component {
                     <th>ID</th>
                     <th>タイトル</th>
                     <th>内容</th>
+                    <th>日時</th>
                     <th>カテゴリ</th>
                     <th>操作</th>
                 </tr>
-                <tr>
-                    <td>aaa</td>
-                    <td>bbb</td>
-                    <td>ccc</td>
-                    <td>ddd</td>
-                    <td>eee</td>
-                </tr>
+                <TodoDateLow todoData={todoData}/>
                 </table>
             </div>
         )
