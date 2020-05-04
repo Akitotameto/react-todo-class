d49557eb4d3ca4799affd061e4bd54896a57e0de
Akitotameto <akito.tameto@gmail.com>
2020-05-04 16:38:43 +0900

    簡易的な画面を作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <div className='contents-1'>
+          <h1>Todo List</h1>
+          <button> List </button>
+          <button> Set </button>
+        </div>
+
+        <table border='1' className='table-1'>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>内容</th>
+            <th>カテゴリ</th>
+            <th>操作</th>
+          </tr>
+          <tr>
+            <td>aaa</td>
+            <td>bbb</td>
+            <td>ccc</td>
+            <td>ddd</td>
+            <td>eee</td>
+          </tr>
+        </table>
+
+      </div>
+    )
+  }
 }
 
 export default App;
